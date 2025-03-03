f669b891e7233bca0744e182162d21385cbe643d
Deepanshu malviya <deepanshumalviya58@gmail.com>
2025-03-03 20:40:54 +0530

    [Deepanshu malviya]- Add. Ability to ensure Valid Contacts are added...

--- address-book-using-js/AddressBook.js
+++ address-book-using-js/AddressBook.js
@@ -1,14 +1,53 @@
 // Class to define a Contact in the Address Book
 class Contact {
     constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
+        this.firstName = this.validateName(firstName, "First Name");
+        this.lastName = this.validateName(lastName, "Last Name");
+        this.address = this.validateMinLength(address, 4, "Address");
+        this.city = this.validateMinLength(city, 4, "City");
+        this.state = this.validateMinLength(state, 4, "State");
+        this.zip = this.validateZip(zip);
+        this.phoneNumber = this.validatePhone(phoneNumber);
+        this.email = this.validateEmail(email);
+    }
+
+    validateName(name, fieldName) {
+        let nameRegex = /^[A-Z][a-zA-Z]{2,}$/;
+        if (!nameRegex.test(name)) {
+            throw new Error(`${fieldName} is invalid. Must start with a capital letter and have at least 3 characters.`);
+        }
+        return name;
+    }
+
+    validateMinLength(value, minLength, fieldName) {
+        if (value.length < minLength) {
+            throw new Error(`${fieldName} must have at least ${minLength} characters.`);
+        }
+        return value;
+    }
+
+    validateZip(zip) {
+        let zipRegex = /^[1-9][0-9]{5}$/;
+        if (!zipRegex.test(zip)) {
+            throw new Error(`Zip is invalid. Should be a 6-digit number starting with non-zero.`);
+        }
+        return zip;
+    }
+
+    validatePhone(phoneNumber) {
+        let phoneRegex = /^[6-9][0-9]{9}$/;
+        if (!phoneRegex.test(phoneNumber)) {
+            throw new Error(`Phone number is invalid. Should be a valid 10-digit Indian number.`);
+        }
+        return phoneNumber;
+    }
+
+    validateEmail(email) {
+        let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if (!emailRegex.test(email)) {
+            throw new Error(`Email is invalid.`);
+        }
+        return email;
     }
 
     // Method to display contact details
@@ -17,16 +56,18 @@ class Contact {
     }
 }
 
-// Example contact creation
-let contact1 = new Contact(
-    "Deepanshu",
-    "Malviya",
-    "456 Colony",
-    "Bhopal",
-    "MP",
-    "462023",
-    "9876543210",
-    "deepanshu@example.com"
-);
-
-console.log(contact1.display());
+try {
+    let contact1 = new Contact(
+        "Deepanshu",
+        "Malviya",
+        "456 Colony",
+        "Bhopal",
+        "MP",
+        "462023",
+        "9876543210",
+        "deepanshu@example.com"
+    );
+    console.log(contact1.display());
+} catch (error) {
+    console.error(error.message);
+}
